45c677d8f960105e2a2c24ea1edfd90d1a6598ee
Stephan Richter <stephan.richter@gmail.com>
2007-10-31 20:15:47 +0000

    Fix deprecation warnings and some package data cleanup.

--- README.txt
+++ README.txt
@@ -1,23 +1,10 @@
+=====================================
 Extensions for the Zope 3 testbrowser
 =====================================
 
 This package is intended to provide extended versions of the Zope 3
 testbrowser. Especially those extensions that introduce dependencies to more
 external products, like lxml.
-
-Changes
--------
-
-1.0.3
-~~~~~
-
-- Remedy brown-bag 1.0.2-release: Someone already released 1.0.2-r* instead of
-  a .dev release making the 1.0.2 final too old. :/
-
-1.0.2
-~~~~~
-
-- Helping libxml2 with some encoding guesswork.
 
 Extension: lxml-support
 -----------------------
--- setup.py
+++ setup.py
@@ -1,24 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3c.viewlet package
+
+$Id$
+"""
+import os
 from setuptools import setup, find_packages
 
-setup(
-    name='z3c.etestbrowser',
-    version='1.0.3',
-    author='Christian Theune',
-    author_email='ct@gocept.com',
-    url='http://svn.zope.org/z3c.etestbrowser/trunk/',
-    description="""\
-Extensions for zope.testbrowser.
-""",
-    long_description = open('README.txt').read(),
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-    zip_safe=False,
-    license='ZPL 2.1',
-    extras_require=dict(test=['zope.app.testing', 'zope.app.zcmlfiles',
-                              'zope.app.securitypolicy', 'zope.app.server']),
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='z3c.etestbrowser',
+      version = '1.0.4',
+      author='Christian Theune',
+      author_email='ct@gocept.com',
+      description='Extensions for zope.testbrowser',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 testbrowser lxml",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/z3c.etestbrowser',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['z3c'],
+      extras_require=dict(test=['zope.app.testing',
+                                'zope.app.zcmlfiles',
+                                'zope.app.securitypolicy',
+                                'zope.app.server']),
     install_requires=['setuptools',
                       'lxml<2.0-dev',
                       'zope.testbrowser'
                      ],
-)
+      include_package_data = True,
+      zip_safe = False,
+      )

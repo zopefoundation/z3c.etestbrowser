7898d4743dd27dcd93e7918e1eab9f3eae79bc4a
Michael Howitz <mh@gocept.com>
2010-01-18 19:53:41 +0000

    Back to development: 1.3.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.etestbrowser',
-      version = '1.3.1',
+      version = '1.3.2dev',
       author='Christian Theune',
       author_email='ct@gocept.com',
       description='Extensions for zope.testbrowser',

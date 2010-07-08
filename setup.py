2a19571a9db36011383c37b01b5e3fb5e67cb595
Thomas Lotze <tl@gocept.com>
2010-07-08 16:29:19 +0000

    Back to development: 1.4.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.etestbrowser',
-      version = '1.4.0',
+      version = '1.4.1dev',
       author='Christian Theune',
       author_email='ct@gocept.com',
       description='Extensions for zope.testbrowser',

0e1eb7c484a67362c1f5b42464483fddce95290f
Michael Howitz <mh@gocept.com>
2010-08-22 12:15:30 +0000

    Preparing release 1.5.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.etestbrowser',
-      version = '1.4.1dev',
+      version='1.5.0',
       author='Christian Theune',
       author_email='ct@gocept.com',
       description='Extensions for zope.testbrowser',

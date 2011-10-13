2a25ebeef4749c6a28ede99f5559da2aa527d1d8
Michael Howitz <mh@gocept.com>
2011-10-13 09:34:31 +0000

    Renamed ``z3c.etestbrowser.wsgi.ExtendedTestBrowser`` to ``Browser`` for equality with ``zope.testbrowser`` but kept ``ExtendedTestBrowser`` for backwards compatibility.

--- src/z3c/etestbrowser/wsgi.py
+++ src/z3c/etestbrowser/wsgi.py
@@ -17,10 +17,13 @@ import z3c.etestbrowser.browser
 import zope.testbrowser.wsgi
 
 
-class ExtendedTestBrowser(zope.testbrowser.wsgi.Browser,
+class Browser(zope.testbrowser.wsgi.Browser,
                           z3c.etestbrowser.browser.ExtendedTestBrowser):
     """An extended testbrowser implementation.
 
     Based on zope.testbrowser.wsgi.Browser.
 
     """
+
+# Just for backwards compatibility with versions before 2.0:
+ExtendedTestBrowser = Browser
